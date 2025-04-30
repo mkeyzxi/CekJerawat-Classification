8d3039698c4621349f27b9005ebc4c8a1bc37a3b
mkeyzxi <muhmakbul6@gmail,com>
2025-04-30 17:47:33 +0800

    view(header): membuat folder components serta file heder berfungsi dengan baik

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+// components/HeaderComponent.js
+class HeaderComponent extends HTMLElement {
+	connectedCallback() {
+		this.innerHTML = `
+		<header class="flex justify-between items-center px-6 md:px-12 py-6">
+		  <div class="text-2xl font-bold">
+			<span class="text-pink-400">Acne</span>
+			<span class="text-sky-400">Scan</span>
+		  </div>
+		  <nav class="space-x-6 text-sm font-medium hidden md:flex">
+			<a class="bg-pink-400 text-white px-4 py-2 rounded-md hover:bg-pink-500 transition" href="#">
+			  Home
+			</a>
+			<a class="text-pink-300 hover:text-pink-400 transition flex justify-center items-center" href="#">
+			  Upload
+			</a>
+			<a class="text-pink-300 hover:text-pink-400 transition flex justify-center items-center" href="#" >
+			  About Us
+			</a>
+		  </nav>
+		</header>
+	  `
+	}
+}
+
+customElements.define('header-component', HeaderComponent)
